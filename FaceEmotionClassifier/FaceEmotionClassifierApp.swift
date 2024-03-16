c1c935bc2cee5af14bf577b522ec80d18dd4f5dc
Sharan Thakur <sharandt19@live.com>
2024-03-16 13:54:55 +0530

    Initial Commit

---
+++ FaceEmotionClassifier/FaceEmotionClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceEmotionClassifierApp.swift
+//  FaceEmotionClassifier
+//
+//  Created by Sharan Thakur on 16/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceEmotionClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
